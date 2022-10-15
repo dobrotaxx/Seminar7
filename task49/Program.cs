02eca32ab5ac6fefcd65f61576dbb1e0ce00d354
Semyon <tonkih.sem@mail.ru>
2022-10-15 23:43:06 +0500

    added task49

---
+++ task49/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+и замените эти элементы на их квадраты.
+Например, изначально массив выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+*/
+
+void PrintColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+void print2DArray(int[,] arrayTwoPrint)
+{
+    Console.Write("\t");
+    for (int i = 0; i < arrayTwoPrint.GetLength(1); i++)
+    {
+        PrintColorData(i + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayTwoPrint.GetLength(0); i++)
+    {
+        PrintColorData(i + "\t");
+        for (int j = 0; j < arrayTwoPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayTwoPrint[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeToSquareInEvenPosition(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i, j] *= matrix[i, j];
+            }
+        }
+    }
+}
+
+int[,] TwoDArray =
+                    {
+                        {1, 4, 7, 2},
+                        {5, 9, 2, 3},
+                        {8, 4, 2, 4}
+                    };
+print2DArray(TwoDArray);
+Console.WriteLine();
+ChangeToSquareInEvenPosition(TwoDArray);
+print2DArray(TwoDArray);
