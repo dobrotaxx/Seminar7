dc4ce415456e7d8dcceb3b30e4f405a7decae787
Semyon <tonkih.sem@mail.ru>
2022-10-15 23:46:03 +0500

    added task49

--- task49/Program.cs
+++ task49/Program.cs
@@ -35,14 +35,11 @@ void print2DArray(int[,] arrayTwoPrint)
 
 void ChangeToSquareInEvenPosition(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i+=2)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j+=2)
         {
-            if (i % 2 == 0 && j % 2 == 0)
-            {
                 matrix[i, j] *= matrix[i, j];
-            }
         }
     }
 }
