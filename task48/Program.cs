17f184aa7463484e6a413f3bc4523f04a288c42c
Semyon <tonkih.sem@mail.ru>
2022-10-15 23:24:18 +0500

    added task48

---
+++ task48/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Задача 48: Задайте двумерный массив размера m на n, 
+каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5
+*/
+
+int[,] generate2DArray(int height, int width, int randomStart, int randomEnd)
+{
+    int[,] twoDArray = new int[height, width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            twoDArray[i, j] = new Random().Next(randomStart, randomEnd + 1);
+        }
+    }
+    return twoDArray;
+}
+
+void PrintColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+void print2DArray(int[,] arrayTwoPrint)
+{
+    Console.Write("\t");
+    for (int i = 0; i < arrayTwoPrint.GetLength(1); i++)
+    {
+        PrintColorData(i + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayTwoPrint.GetLength(0); i++)
+    {
+        PrintColorData(i + "\t");
+        for (int j = 0; j < arrayTwoPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayTwoPrint[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] generatedArray = generate2DArray(7, 5, -100, 100);
+print2DArray(generatedArray);
